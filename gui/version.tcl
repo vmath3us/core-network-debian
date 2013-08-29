3c4bd21a26e3027b3943812804b6396f13829cf9
Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
2013-08-28 23:34:13 -0300

    Adding upstream version 4.6~rc5.

---
+++ gui/version.tcl
@@ -0,0 +1,10 @@
+#
+# Copyright 2005-2013 the Boeing Company.
+# See the LICENSE file included in this distribution.
+#
+
+#
+# define the version number and release date here
+#
+set CORE_VERSION 4.6rc5
+set CORE_VERSION_DATE 20130828
